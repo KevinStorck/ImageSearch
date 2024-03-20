05a9f6cba1d4ea2e49bda0f255b34cb880d1ad53
KevinStorck <kevin.storck@hotmail.com>
2024-03-21 00:21:13 +0100

    Updated README with intructions for project setup

--- client/src/services/ImageSearchService.ts
+++ client/src/services/ImageSearchService.ts
@@ -1,12 +1,11 @@
 import axios from "axios";
 import { IImageSearchData } from "../models/IImageSearchData";
 
-const ENGINE_ID = import.meta.env.VITE_ENGINE_ID;
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 export const ImageSearch = async (input: string): Promise<IImageSearchData> => {
   let response = await axios.get<IImageSearchData>(
-    `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${ENGINE_ID}&searchType=image&q=${input}`
+    `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=***REMOVED***&searchType=image&q=${input}`
   );
   console.log(response);
 
